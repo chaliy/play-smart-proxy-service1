166206ca9e955fc873d09a17c77fddc1e26b97ed
Mike Chaliy <mike@chaliy.name>
2016-09-01 17:47:18 +0300

    Added more operations to employees API

--- api/controllers/employee.js
+++ api/controllers/employee.js
@@ -1,10 +1,30 @@
-let _ = require('lodash');
-let employees = require('../../data/employees.json');
+const _ = require('lodash');
+const minimatch = require('minimatch');
+const employees = require('../../data/employees.json');
 
 module.exports.list = (req, res, next) => {
-  res.json(_.take(employees, 4));
+  let results = _(employees).take(4);
+  return res.json(results);
+}
+
+module.exports.item = (req, res, next) => {
+
+  let employee = _.find(employees, { id: req.swagger.params.id.value });
+  if (employee){
+      return res.json(employee);
+  }
+
+  return res.status(404).send('Sorry cant find that!');
+}
+
+module.exports.search = (req, res, next) => {
+  let query = req.swagger.params.q.value;
+  let results = _(employees)
+                  .filter(empl => minimatch(empl.fullName, query))
+                  .take(4);
+  return res.json(results);
 }
 
 module.exports.create = (req, res) => {
-  res.json(`Hello world!`);
+  return res.json(`Hello world!`);
 }
